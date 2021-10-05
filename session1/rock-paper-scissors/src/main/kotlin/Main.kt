f76f4b53537ebf042fb184e90ba36c495993e2a9
BrittHellinckx <britt.hellinckx@student.ehb.be>
2021-10-05 12:27:56 +0200

    Rock paper scissors

---
+++ session1/rock-paper-scissors/src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+fun main(){
+    var options = arrayOf("rock", "paper", "scissors")
+    var optionsPc = options[(0..2).random()]
+    var userInput = readLine()
+
+    if(userInput== "rock"){
+        when (optionsPc){
+            "rock" -> println("Draw: Computer chose $optionsPc")
+            "paper" ->println("You lose: Computer chose $optionsPc")
+            "scissors"->println("You win: Computer chose $optionsPc")
+        }
+    }
+    if(userInput== "paper"){
+        when (optionsPc){
+            "paper" -> println("Draw: Computer chose $optionsPc")
+            "scissors" ->println("You lose: Computer chose $optionsPc")
+            "rock"->println("You win: Computer chose $optionsPc")
+        }
+    }
+    if(userInput== "scissors"){
+        when (optionsPc){
+            "scissors" -> println("Draw: Computer chose $optionsPc")
+            "rock" ->println("You lose: Computer chose $optionsPc")
+            "paper"->println("You win: Computer chose $optionsPc")
+        }
+    }
+}
