99d584b218382d0976681595a9167fead8aa376d
BrittHellinckx <britt.hellinckx@student.ehb.be>
2021-10-05 11:47:32 +0200

    hello world dynamic

--- session1/hello-world/src/main/kotlin/Main.kt
+++ session1/hello-world/src/main/kotlin/Main.kt
@@ -1,3 +1,9 @@
 fun main(){
-    println("Hello world")
+    var names = arrayOf("Britt", "Liese", "Rebecca", "Arno", "Hannelore")
+    for (name in names){
+        printName(name)
+    }
+}
+fun printName(name: String){
+    println("Hello $name")
 }
