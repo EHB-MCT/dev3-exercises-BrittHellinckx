bea27a6c87fbc07a5a6cf065d7cfd7a77f6bb622
BrittHellinckx <britt.hellinckx@student.ehb.be>
2021-10-12 12:47:50 +0200

    Adding challenge 3 & 4 and the beginning of 5

--- session2/test/src/main/kotlin/Main.kt
+++ session2/test/src/main/kotlin/Main.kt
@@ -47,5 +47,45 @@ fun challengeTwo(){
 }
 fun challengeThree(){
     println("Perfect amount of coffee")
+    println("Now it's time to go to school, choose you way of transportation")
+    println("on foot - by bike - with the train")
 
+    val userInput = readLine()
+
+    when userInput{
+        "on foot"->{
+            printlin("Oh no, you don't have enough energy, go back and drink some more coffee")
+            challengeTwo()
+            }
+        "by bike"-> challengeFour()
+        "with the train"->{
+            println("That wasn't very smart... Of course there are train delays and so you misses school")
+            gameOver()
+            }
+    }
+}
+val pcGuess = (1..100).random()
+fun challengeFour(){
+    println("Yay! You made it on time. Now quickly, where do you need to be?")
+    println("Choose a classroom between 1 and 100")
+
+    val userInput = readLine ()
+
+    if (userInput == pcGuess){
+        challengeFive()
+    }else if(userInput<pcGuess){
+        println("Akward, you entered the wrong classroom. Your classroom is further down the hall")
+        challengeFour()
+    }else if(userInput> pcGuess){
+        println("Akward, you entered the wrong classroom. You already passed your classroom")
+        challengeFour()
+    }
+}
+fun challengeFive(){
+    println("You picked the right one, now time for class")
+
+    //listen/silent
+    //study/dusty
+    //learn/renal
+    //read/dear
 }
