aaa918223e196b7ef2803674833e3b4e972f7255
BrittHellinckx <britt.hellinckx@student.ehb.be>
2021-10-12 11:23:27 +0200

    Making challenge one and two

--- session2/adventuregame/src/main/kotlin/Main.kt
+++ session2/adventuregame/src/main/kotlin/Main.kt
@@ -4,6 +4,7 @@ fun main(){
 }
 fun gameOver(){
     println("Ready to try again?")
+    main()
 }
 fun challengeOne(){
     println("It's 6am and your alarm clock is ringing")
@@ -20,7 +21,29 @@ fun challengeOne(){
         gameOver()
     }
 }
-
 fun challengeTwo(){
+    println("Now time for breakfast, how many cups of coffee are you going to drink?")
+    println("Are you going to choose little coffe (1-3 cups) or a lot of coffee (4-6 cups)?")
+    val pcRandom = (0..6).random()
+    val userInput = readLine()
 
+    if(pcRandom<=3){
+        when(userInput)
+            "little coffee"-> challengeThree()
+            "a lot of coffee"->{
+                println("You drank too much coffee and can't focus on school")
+                gameOver()
+        }
+    }else if(pcRandom=>4){
+        when(userInput)
+            "a lot of coffee" -> challengeThree()
+            "little coffee"->{
+        println("You drank too little coffee and fell asleep again")
+        gameOver()
+        }
+    }
+}
+fun challengeThree(){
+    println("Perfect amount of coffee")
+    
 }
