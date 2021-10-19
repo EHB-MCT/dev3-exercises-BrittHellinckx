b5b87c95c53e7363152ae2236d1c3dbd2092253d
BrittHellinckx <britt.hellinckx@student.ehb.be>
2021-10-19 09:41:12 +0200

    Adding class and first course

--- session3/clientbrief/src/main/kotlin/Course.kt
+++ session3/clientbrief/src/main/kotlin/Course.kt
@@ -1,4 +1,12 @@
 class Course(
+    val name: String,
+    val module: String,
+    val teacher: String,
+    val credits: Int,
+    val year: Int,
+    val semester: Int,
+    val required: Boolean,
+    val description: String
 
 ) {
 }
--- session3/clientbrief/src/main/kotlin/Main.kt
+++ session3/clientbrief/src/main/kotlin/Main.kt
@@ -1,3 +1,3 @@
 fun main(){
-println("out folder")
+    val design1 = Course("design1","design","Jan", 5, 1,1,true,"Learning how to work with adobe" )
 }
