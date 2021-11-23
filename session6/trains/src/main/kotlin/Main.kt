722cbf799880149cb94ffae6aba72381e094157b
BrittHellinckx <britt.hellinckx@student.ehb.be>
2021-11-23 11:55:45 +0100

    Collecting and displaying data

--- session6/trains/src/main/kotlin/Main.kt
+++ session6/trains/src/main/kotlin/Main.kt
@@ -9,15 +9,22 @@ fun main(){
     val connectionProps = Properties();
     connectionProps["user"] = credentials.databaseUser
     connectionProps["password"] = credentials.databasePassword
-
     // Create the connection: this will allow us to run queries on it later
     val connection =  DriverManager.getConnection(
         "jdbc:" + "mysql" + "://" +
                 "dt5.ehb.be" +
-                ":" + "3306 (this is your port, this is de default)" + "/" +
+                ":" + "3306" + "/" +
                 credentials.databaseName,
         connectionProps)
 
-    //
-
+    //Collect data
+    val statement = connection.prepareStatement("SELECT * from 6_rides" + " "+
+                "LEFT JOIN 6_trains ON 6_trains.id = 6_rides.train_id" +" "+
+                "LEFT JOIN 6_cities ON 6_cities.id = 6_rides.arrival_city_id" )
+    val result = statement.executeQuery()
+    while(result.next()){
+        println("At: "+result.getString("departure_time"))
+        println(result.getString("train_name"))
+        println("To: " + result.getString("city_name"))
+    }
 }
