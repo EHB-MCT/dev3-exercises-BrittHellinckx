ca1248d6af283ce1a6e485d7a57e73da9ec2705f
BrittHellinckx <britt.hellinckx@student.ehb.be>
2021-11-23 13:02:49 +0100

    adding security (and changing date check)

--- session6/trains/src/main/kotlin/Main.kt
+++ session6/trains/src/main/kotlin/Main.kt
@@ -21,16 +21,19 @@ fun main(){
     //Ask city
     println("Waar wil je naartoe?")
     val userCity = readLine()
-    val currentDate = LocalDate.now()
+    //val currentDate = LocalDate.now()
 
     //Collect data for city
     val statement = connection.prepareStatement("SELECT * from 6_rides" +
             " LEFT JOIN 6_trains ON 6_trains.id = 6_rides.train_id" +
             " LEFT JOIN 6_cities ON 6_cities.id = 6_rides.arrival_city_id" +
-            " WHERE city_name = '$userCity' AND departure_time LIKE '$currentDate%'" +
+            " WHERE city_name = ? AND departure_time < NOW()" +
+            //time: '$currentDate%'
             " ORDER BY (departure_time) ASC" +
             " LIMIT 3")
+    statement.setString(1, userCity)
     val result = statement.executeQuery()
+
 
     //Print data
     while(result.next()){
