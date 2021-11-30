daa566cfe702c314025696877c99d5608428d952
BrittHellinckx <britt.hellinckx@student.ehb.be>
2021-11-30 10:39:48 +0100

    added all words to duolingo
    added 10 words in french, english and dutch

---
+++ session7/duolingo/src/main/kotlin/Duolingo.kt
@@ -0,0 +1,24 @@
+class Duolingo {
+    init{
+        val words = arrayOf(Word("cat", "kat", "engels"),
+                            Word("chat", "kat", "frans"),
+                            Word("bunny", "konijn", "engels"),
+                            Word("lapin", "konijn", "frans"),
+                            Word("dog", "hond", "engels"),
+                            Word("chien", "hond", "frans"),
+                            Word("fish", "vis", "engels"),
+                            Word("poisson", "vis", "frans"),
+                            Word("horse", "paard", "engels"),
+                            Word("cheval", "paard", "frans"),
+                            Word("goat", "geit", "engels"),
+                            Word("chevre", "geit", "frans"),
+                            Word("chicken", "kip", "engels"),
+                            Word("poulet", "kip", "frans"),
+                            Word("mouse", "muis", "engels"),
+                            Word("souris", "muis", "frans"),
+                            Word("donkey", "ezel", "engels"),
+                            Word("ane", "ezel", "frans"),
+                            Word("snake", "slang", "engels"),
+                            Word("serpent", "slang", "frans"))
+    }
+}
