66517914f704042468131e8aacb4ac882c7818b0
BrittHellinckx <britt.hellinckx@student.ehb.be>
2021-12-07 12:11:50 +0100

    finished part 2

--- session8/duolingo/src/main/kotlin/Duolingo.kt
+++ session8/duolingo/src/main/kotlin/Duolingo.kt
@@ -40,14 +40,24 @@ class Duolingo (val lan: String){
     fun play(randomWords:MutableList<Word>){
         while(randomWords.isNotEmpty()){
             val selectedWord = randomWords.random()
+                //Question for user
                 println("Vertaal " + selectedWord.original + " in het het Nederlands")
                 val userInput = readLine()
+                //User got answer correct
                 if(userInput == selectedWord.translated){
+                    //lowers diff
+                    selectedWord.diff = selectedWord.diff - 1
+                    //removes word
                     randomWords.remove(selectedWord);
-                    println("Dat is juist!")
-                }else{
-                    println("Fout! De juiste vertaling is ${selectedWord.translated}")
+                    println("Dat is juist! woord moeilijkheid is ${selectedWord.diff}")
                 }
+                //user got answer wrong
+                else{
+                    //ups diff
+                    selectedWord.diff = selectedWord.diff + 2
+                    println("Fout! De juiste vertaling is ${selectedWord.translated}, woord moeilijkheid is ${selectedWord.diff}")
+                }
+            //counts how many words to go
             when{
                randomWords.count()==0 -> println("Goed gedaan!")
                 randomWords.count()>0 -> println("Nog " + randomWords.count())
